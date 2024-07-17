804678321041d5ae32ea393af294c8cf4beefdf7
Amirreza <amirrezakashipazha@gmail.com>
2024-07-17 09:41:18 -0700

    fix:back to the original verstion

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require('cors'); 
+const app = express();
+const port = 3001;
+
+const crawler = require("./crawlers/crawler");
+const { ROUTES } = require("./ROUTES");
+
+app.use(express.static("public"));
+app.use(cors({
+    origin: "*",
+    credentials: false
+}));
+
+// --------------- crawlers Routes ------------------
+for (let index = 0; index < ROUTES.length; index++) {
+    const route = ROUTES[index];
+
+    app.get(route.route, async (req, res) => {
+        crawler(route.crawler_url, route.crawler, req.params).then((data) => {
+            res.send(route.response(data));
+        });
+    });
+}
+// --------------- --------------- ------------------
+
+app.get("/", (req, res) => {
+    const baseURL = req.get("host");
+
+    res.send({
+        message: "server is running",
+        github: "https://github.com/Amir-Alipour/time.ir-crawler",
+        routes: [
+            {
+                route: "/year",
+                description: "the current year and it months with events",
+                url: `https://${baseURL}/year`,
+            },
+        ],
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
