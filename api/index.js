836c1a35b824deb710e365cf50acc51c18e39cbe
Amirreza <amirrezakashipazha@gmail.com>
2024-07-17 09:38:09 -0700

    fix:export the api

--- api/index.js
+++ api/index.js
@@ -1,46 +1,27 @@
-const express = require("express");
-const cors = require('cors'); 
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3001;
 
-const crawler = require("../crawlers/crawler");
-const { ROUTES } = require("../ROUTES");
+const crawler = require('../crawlers/crawler');
+const { ROUTES } = require('../ROUTES');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(cors({
-    origin:"*",
-    credentials:false
+    origin: "*",
+    credentials: false
 }));
-// --------------- crawlers Routes ------------------
+
+// Define routes based on your ROUTES configuration
 for (let index = 0; index < ROUTES.length; index++) {
     const route = ROUTES[index];
-
-    app.get(route.route, async (_, res) => {
-        crawler(route.crawler_url, route.crawler,_.params).then((data) => {
-            res.send(route.response(data));
-        });
+    app.get(route.route, async (req, res) => {
+        try {
+            const data = await crawler(route.crawler_url, route.crawler, req.query);
+            res.json(route.response(data));
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     });
 }
-// --------------- --------------- ------------------
 
-app.get("/", (req, res) => {
-    const baseURL = req.get("host");
-
-    res.send({
-        message: "server is running",
-        github: "https://github.com/Amir-Alipour/time.ir-crawler",
-        routes: [
-          
-            {
-                route: "/year",
-                description: "the current year and it months with events",
-                url: `https://${baseURL}/year`,
-            },
-           
-        ],
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = app;
